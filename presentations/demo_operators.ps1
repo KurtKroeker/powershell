8872851a505a7f25b0f07002798d5e600f6035d8
Kurt Kroeker <kurt.kroeker@energycap.com>
2019-11-13 08:33:23 -0500

    Add Where-Object to operators demo

--- presentations/demo_operators.ps1
+++ presentations/demo_operators.ps1
@@ -6,12 +6,12 @@ Set-location C:\powershell\presentations
 
 # [int]$matches = 0
 
-# TODO: output any cds from the 80s
-# TODO: find bob dylan cds
+# # TODO: output any cds from the 80s
+# # TODO: find bob dylan cds
 
 # foreach($cd in $cds.catalog.cd){
-# 	if($cd.year -ge 1980 -and $cd.year -le 1989 -and $cd.ARTIST){
-# 		#$cd
+# 	if($cd.year -ge 1980 -and $cd.year -le 1989 -and $cd.ARTIST -like "*dylan*"){
+# 		$cd
 # 		$matches++
 # 	}
 # }
@@ -19,5 +19,7 @@ Set-location C:\powershell\presentations
 # "$matches matches for the conditions"
 
 # TODO: show match/notmatch
-# TODO: show -replace, -split
 # TODO: whatif for destructive actions
+
+# TODO :fix me
+#$cds.CATALOG.CD | Where-Object { $_.artist -match "dylan" } | Select-Object -Property title
